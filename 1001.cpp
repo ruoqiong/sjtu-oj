634eddb76c8b86d45c56f703e07fedf3761a37d3
Your Name <you@example.com>
2017-09-15 16:25:37 +0800

    accept

--- 1001.cpp
+++ 1001.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 int main()
 {
-    int a,b;
-    cin>>a>>b;
-    cout<<a+b;
+    int a,b,c;
+    cin>>a>>b>>c;
+    int d[c];
+    int i,j;
+    j=0;
+    for(i=0;i<c;i++){
+        cin>>d[c];
+        if(d[c]<=a+b){
+            j++;
+        }
+    }
+    cout<<j;
     return 0;
 }
--- 1002.cpp
+++ 1002.cpp
@@ -1,18 +1,41 @@
+//采用前缀和的思想
 #include<iostream>
 using namespace std;
 int main()
 {
-    int a,b,c;
-    cin>>a>>b>>c;
-    int d[c];
+    int L,W,r,a,b;
+    cin>>L>>W;
+    int m[L][W],s[L][W];
     int i,j;
-    j=0;
-    for(i=0;i<c;i++){
-        cin>>d[c];
-        if(d[c]<=a+b){
-            j++;
+    for(i=0;i<L;i++){
+        for(j=0;j<W;j++){
+            cin>>m[i][j];
+            if(i==0&&j==0)
+                s[i][j]=m[i][j];
+            else if(i>0&&j==0)
+                s[i][j]=s[i-1][j]+m[i][j];
+            else if(j>0&&i==0)
+                s[i][j]=s[i][j-1]+m[i][j];
+            else
+                s[i][j]=s[i][j-1]+m[i][j]+s[i-1][j]-s[i-1][j-1];
         }
     }
-    cout<<j;
+    cin>>a>>b;
+    int temp,result=0;
+    for(i=0;i<=L-a;i++){
+        for(j=0;j<=W-b;j++){
+            if(i==0&&j==0)
+                temp=s[a+i-1][b+j-1];
+            else if(i>0&&j>0)
+                temp=s[a+i-1][b+j-1]-s[a+i-1][j-1]-s[i-1][b+j-1]+s[i-1][j-1];
+            else if(i==0&&j>0)
+                temp=s[a+i-1][b+j-1]-s[a+i-1][j-1];
+            else
+                temp=s[a+i-1][b+j-1]-s[i-1][b+j-1];
+            if(result<temp)
+                result=temp;
+        }
+    }
+    cout<<result;
     return 0;
 }
